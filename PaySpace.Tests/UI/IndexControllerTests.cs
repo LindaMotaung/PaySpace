0a1eb807aaec1be2cd1311ccd3699be4e5071545
LMOTAUN <linda.motaung@mercedes-benz.com>
2023-08-14 10:49:22 +0200

    feat: Added some more tests

--- PaySpace.Tests/UI/IndexControllerTests.cs
+++ PaySpace.Tests/UI/IndexControllerTests.cs
@@ -1,17 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using NUnit.Framework;
+using PaySpace.Pages;
 
 namespace PaySpace.Tests.UI
 {
     [TestFixture]
     public class IndexControllerTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void MapPostalCodes_ValidCode_ReturnsCorrectId()
         {
-            
+            // Arrange
+            var model = new IndexModel();
+
+            // Act
+            var result = model.MapPostalCodes("7441");
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void MapPostalCodes_InvalidCode_ThrowsException()
+        {
+            // Arrange
+            var model = new IndexModel();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => model.MapPostalCodes(null));
+            Assert.Throws<ArgumentNullException>(() => model.MapPostalCodes(""));
+            Assert.Throws<ArgumentNullException>(() => model.MapPostalCodes("InvalidCode"));
+        }
+
+        [Test]
+        public void OnPost_ValidModelState_ReturnsPageResult()
+        {
+            // Arrange
+            var model = new IndexModel();
+            model.PostalCode = "7441";
+            model.Income = 50000;
+
+            // Act
+            var result = model.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+        }
+
+        [Test]
+        public void OnPost_InvalidModelState_ReturnsPageResultWithErrors()
+        {
+            // Arrange
+            var model = new IndexModel
+            {
+                PostalCode = null,
+                Income = -5000 // Invalid income
+            };
+
+            // Act
+            var result = model.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+            Assert.IsFalse(model.ModelState.IsValid);
         }
     }
 }
--- PaySpace/Pages/Index.cshtml.cs
+++ PaySpace/Pages/Index.cshtml.cs
@@ -10,16 +10,59 @@ namespace PaySpace.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public int Income { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        [BindProperty]
+        public string PostalCode { get; set; }
+
+        public int PostalCodeId { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public int MapPostalCodes(string PostalCode)
+        {
+            if (string.IsNullOrEmpty(PostalCode))
+            {
+                throw new ArgumentNullException(nameof(PostalCode), "Postal code should not be empty or null.");
+            }
+
+            this.PostalCodeId = PostalCode switch
+            {
+                "7441" => 1,
+                "A100" => 2,
+                "7000" => 3,
+                "1000" => 4,
+                _ => throw new ArgumentNullException(nameof(PostalCode),
+                    "Postal code should be either 7441, A100, 7000 or 1000")
+            };
+
+            return this.PostalCodeId;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Ensure PostalCode has the correct value
+            if (string.IsNullOrEmpty(PostalCode))
+            {
+                ModelState.AddModelError("PostalCode", "Postal code should not be empty or null.");
+                return Page();
+            }
+
+            MapPostalCodes(PostalCode);
+            // This method will be executed when the form is submitted using POST.
+
+            // Do something with the entered data (e.g., save to a database or process it).
+
+            return Page(); // Return the same page after form submission.
         }
     }
 }
